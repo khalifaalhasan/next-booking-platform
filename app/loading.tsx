79a7cadcbbeaa987efd3b231cdd5c310981eb73c
NakamaShp <khlfaalhsn5@gmail.com>
2025-11-28 21:32:20 +0700

    feat(ui): add global loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,28 +1,5 @@
-import { Skeleton } from "@/components/ui/Skeleton";
+import SpinnerLoading from "@/components/ui/SpinnerLoading";
 
-export default function GlobalLoading() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      {/* Header Skeleton */}
-      <div className="flex justify-between items-center mb-8">
-        <Skeleton className="h-8 w-48" />
-        <Skeleton className="h-10 w-32" />
-      </div>
-
-      {/* Grid Skeleton (Cocok untuk halaman list maupun detail) */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div className="md:col-span-2 space-y-4">
-          <Skeleton className="h-64 w-full rounded-xl" />
-          <div className="space-y-2">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-5/6" />
-            <Skeleton className="h-4 w-4/6" />
-          </div>
-        </div>
-        <div className="md:col-span-1">
-          <Skeleton className="h-80 w-full rounded-xl" />
-        </div>
-      </div>
-    </div>
-  );
+export default function Loading() {
+  return <SpinnerLoading />;
 }
